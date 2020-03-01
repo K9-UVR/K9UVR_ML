69262475c56c1a734422ee4b419435fb1e88d095
Konrad <konradkarimi@gmail.com>
2020-03-01 18:32:29 +0100

    I don't know why. Just move on.

--- detect.py
+++ detect.py
@@ -21,23 +21,19 @@ FLAGS = flags.FLAGS
 def main(argv):
 	logging.set_verbosity(logging.INFO)
 	del argv  # Unused.
-#   for i in range(0, FLAGS.num_times):
-#     print('Hello, %s!' % FLAGS.name)
 
-	# image_url = "https://img-9gag-fun.9cache.com/photo/a6N211m_460swp.webp"
 	image_url = "https://img-9gag-fun.9cache.com/photo/aWEbGgZ_460swp.webp"
-	downloaded_image_path = utils.download_and_resize_image(
-		image_url, 1280, 856, False) # false to not display empty img
+	image_url = "/Users/konradkarimi/Downloads/car.jpg"
+	# downloaded_image_path = utils.download_and_resize_image(
+	# 	image_url, 1280, 856, False) # false to not display empty img
 
 	'''	Available models for model detection:
 		"https://tfhub.dev/google/openimages_v4/ssd/mobilenet_v2/1"
 		"https://tfhub.dev/google/faster_rcnn/openimages_v4/inception_resnet_v2/1"
 	'''
-	# module_handle = "https://tfhub.dev/google/openimages_v4/ssd/mobilenet_v2/1"
-	module_handle = "https://tfhub.dev/google/faster_rcnn/openimages_v4/inception_resnet_v2/1"
+	module_handle = "https://tfhub.dev/google/openimages_v4/ssd/mobilenet_v2/1"
 	detector = hub.load(module_handle).signatures['default']
-	# utils.load_img(downloaded_image_path)
-	utils.run_detector(detector, downloaded_image_path)
+	utils.run_detector(detector, image_url)
 
 
 if __name__ == '__main__':
